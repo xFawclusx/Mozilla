df25c0d567590ff1331d7e71113c6edd1dd1e38a
xFawclusx <81125104+xFawclusx@users.noreply.github.com>
2021-04-13 16:44:52 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+let miBoton = document.querySelector('button');
+let miTitulo = document.querySelector( 'h1');
+
+function estableceNombreUsuario() {
+    let miNombre = prompt('Por favor, ingresa tu nombre.');
+    localStorage.setItem('nombre', miNombre);
+    miTitulo.textContent = 'Mozilla es genial, ' + miNombre;
+}
+
+if (!localStorage.getItem('nombre')) {
+    estableceNombreUsuario();
+}
+else {
+    let nombreAlmacenado = localStorage.getItem('nombre');
+    miTitulo.textContent = 'Mozilla es genial,' + nombreAlmacenado;
+}
+
+miBoton.onclick = function() {
+    estableceNombreUsuario();
+}
+
+
